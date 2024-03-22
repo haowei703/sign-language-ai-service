0d41a6efa8f168727f7b45b415c051a13ee033a8
Rem <1436381036@qq.com>
2024-03-22 10:03:39 +0800

    手势识别模块完成

--- main.py
+++ main.py
@@ -2,16 +2,30 @@ import asyncio
 
 import numpy as np
 import websockets
+import struct
+
+from utils.convert_YUV_to_RGB import convert_YUV_to_RGB
+from remain import recognition
 
 
 async def echo(websocket, path):
+    width = 320
+    height = 240
+    i = 1
     async for message in websocket:
         array = np.frombuffer(message, dtype=np.uint8)
-        await websocket.send(f"Echo: {array}")
+        image_size_bytes = array[:8]
+        width, height = struct.unpack('>ii', image_size_bytes)
+
+        array_rgb = convert_YUV_to_RGB(array[8:], width, height)
+
+        text = await recognition(array_rgb)
+        if text is not None:
+            await websocket.send(text)
 
 
 async def main():
-    async with websockets.serve(echo, "localhost", 8765):
+    async with websockets.serve(echo, "0.0.0.0", 10123, max_size=52428800):
         await asyncio.Future()
 
 
---
+++ remain.py
@@ -0,0 +1,49 @@
+import pickle
+import cv2
+import mediapipe as mp
+import numpy as np
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+model_dict = pickle.load(open('./model.p', 'rb'))
+model = model_dict['model']
+
+cap = cv2.VideoCapture(0)
+
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+
+labels_dict = {0: '好', 1: '认', 2: '志', 3: '愿', 4: '者', 5: '识', 6: '次', 7: '6', 8: '5', 9: '爸爸', 10: '妈妈',
+               11: '事', 12: '抽烟', 13: '打电话', 14: '觉得', 15: '先生', 16: '这里', 17: '知道', 18: '走'}
+
+
+async def recognition(frame_rgb):
+    data_aux = []
+    x_ = []
+    y_ = []
+
+    results = hands.process(frame_rgb)
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            for i in range(len(hand_landmarks.landmark)):
+                x = hand_landmarks.landmark[i].x
+                y = hand_landmarks.landmark[i].y
+
+                x_.append(x)
+                y_.append(y)
+
+            for i in range(len(hand_landmarks.landmark)):
+                x = hand_landmarks.landmark[i].x
+                y = hand_landmarks.landmark[i].y
+                data_aux.append(x - min(x_))
+                data_aux.append(y - min(y_))
+
+        prediction = model.predict([np.asarray(data_aux)])
+
+        predicted_character = labels_dict[int(prediction[0])]
+        print(predicted_character)
+
+        return predicted_character
